7e3df763d2c4f8cb9034df3756e7ed061e45470e
martin joshy <martinjoshy28@gmail.com>
2026-05-11 22:20:56 +0530

    feat: psudo code and do the first test of comunication from the bot to the user

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mdp/qrterminal/v3"
 	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
+	"gorm.io/gorm"
 )
 
 func eventHandler(evt interface{}) {
@@ -23,20 +29,93 @@ func eventHandler(evt interface{}) {
 		contextInfo := v.Message.GetExtendedTextMessage().GetContextInfo()
 
 		if contextInfo != nil {
-			quotedId := contextInfo.GetStanzaID()
+			quotedID := contextInfo.GetStanzaID()
 			quotedMessage := contextInfo.GetQuotedMessage()
 
-			fmt.Println("Id : ", quotedId, ", Message : ", quotedMessage)
+			fmt.Println("Id : ", quotedID, ", Message : ", quotedMessage)
 
 		}
+
+		msgUserInfo := v.Info
+		fmt.Println("PhoneNumber :", msgUserInfo.SenderAlt.User, "DisplayName : ", msgUserInfo.PushName, "JID : ", msgUserInfo.Sender.User)
+		LID, phoneNumber := getLIDAndNumberFromEvent(v)
+		message := contextInfo.GetQuotedMessage().String()
+		// listen to only the whitelisted jid as we dont want
+		// no else to use
+
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+		// check if the jid exist in the db
+		user, err := gorm.G[User](DB).Where("l_id = ?", LID).First(ctx)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			err := gorm.G[User](DB).Create(ctx, &User{
+				LID: LID, DisplayName: msgUserInfo.PushName, PhoneNumber: phoneNumber,
+				Messages: []Message{{
+					StanzaID: contextInfo.GetStanzaID(), SentAt: msgUserInfo.Timestamp, Type: MessageTypeText,
+					MessageAttachments: []MessageAttachment{{Body: &message}},
+				}},
+			})
+			serverUnknownErrMsg := "An unknown Error occured, please try after sometime"
+			if err != nil {
+				_, sendErr := client.SendMessage(context.Background(), msgUserInfo.Sender, &waE2E.Message{
+					Conversation: proto.String(serverUnknownErrMsg),
+				})
+			}
+			// sendWelcomeText()
+			_, sendErr := client.SendMessage(context.Background(), msgUserInfo.Sender, &waE2E.Message{
+				Conversation: proto.String("Hello, World!"),
+			})
+
+			if sendErr != nil {
+				fmt.Println(sendErr)
+			}
+		} else {
+			err := gorm.G[Message](DB).Create(ctx, &Message{
+				UserID: user.ID, StanzaID: contextInfo.GetStanzaID(), SentAt: msgUserInfo.Timestamp, Type: MessageTypeText,
+				MessageAttachments: []MessageAttachment{{Body: &message}},
+			})
+			fmt.Println(err)
+		}
+		_, sendErr := client.SendMessage(context.Background(), msgUserInfo.Sender, &waE2E.Message{
+			Conversation: proto.String("Hello, World!"),
+		})
+
+		if sendErr != nil {
+			fmt.Println(sendErr)
+		}
+		// and send welcome
+		// then storeForwards() or else only storeForwards()
+		// send error or success for storeForwards()
 	}
 }
 
-func main() {
-	// |------------------------------------------------------------------------------------------------------|
-	// | NOTE: You must also import the appropriate DB connector, e.g. github.com/mattn/go-sqlite3 for SQLite |
-	// |------------------------------------------------------------------------------------------------------|
+// getLIDFromEvent returns LID and PhoneNumber from the event
+// whatsapp is migrating from JID to LID so it is important to
+// store LID
+func getLIDAndNumberFromEvent(evt *events.Message) (string, string) {
+	if strings.Contains(evt.Info.Sender.String(), "@lid") {
+		return evt.Info.Sender.User, evt.Info.SenderAlt.User
+	} else {
+		// TODO: This block is to be testd as I don't have number that send JID
+		return evt.Info.SenderAlt.String(), evt.Info.Sender.User
+	}
+}
 
+// func sendWelcomeText() string { // welcome text for new user
+// 	panic("Not implimented")
+// }
+
+// func storeMsgForwards(any) error { // think if I should use generics
+// 	// this function should handle all the Forward type case
+// 	// string
+// 	// store the messages with type text and the attachment with body
+// 	// and with tag dump as the default tag
+// 	panic("Not implimented")
+// }
+
+var client *whatsmeow.Client
+
+func main() {
 	dbLog := waLog.Stdout("Database", "DEBUG", true)
 	ctx := context.Background()
 	container, err := sqlstore.New(ctx, "sqlite3", "file:bot.db?_foreign_keys=on", dbLog)
@@ -49,7 +128,7 @@ func main() {
 		panic(err)
 	}
 	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client = whatsmeow.NewClient(deviceStore, clientLog)
 	client.AddEventHandler(eventHandler)
 
 	if client.Store.ID == nil {
